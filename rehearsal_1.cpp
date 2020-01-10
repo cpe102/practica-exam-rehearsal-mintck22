95738f155e703f3fca67cf1975c22f1d03f0ae90
Mint <chawanluck_k@hotmail.com>
2020-01-10 17:08:36 +0700

    1

--- rehearsal_1.cpp
+++ rehearsal_1.cpp
@@ -1,1 +1,30 @@
+#include<iostream>
+#include<string>
 
+using namespace std;
+
+int main(){
+
+    //string name = n[];
+    string name;
+    float GPA = 0;
+
+    cout << "What is your name?: ";
+    cin >> name;
+
+    cout << "What is your GPA?: ";
+    cin >> GPA;
+
+    if(GPA >= 3.50){
+
+        cout << name << " InW Jrim Jrim!!!";
+
+    }else
+    {
+        cout << "Try harder, " << name << "!!!";
+    }
+    
+
+
+    return 0;
+}
