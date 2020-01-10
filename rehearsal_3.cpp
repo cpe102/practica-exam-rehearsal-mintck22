7e9b02a09f0b8d42cd2bb15d6b104a823119aef1
Mint <chawanluck_k@hotmail.com>
2020-01-10 17:09:09 +0700

    3

--- rehearsal_3.cpp
+++ rehearsal_3.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
+#include<cmath>
 
-//Write sumSqrt() here.
+using namespace std;
+
+double sumSqrt(int N){
+    double sum = 0;
+    int i = 1;
+
+    if (N > 0)
+    {   while (i <= N)
+    {
+        sum = sum + 1/pow(i,0.5);
+        i++;
+    }
+       return sum;
+    }else
+    {
+        return 0;
+    }
+    
+    
+}
 
 
 int main()
 {
-    double a = sumSqrt(20);
-    double b = sumSqrt(10);
+    double a = sumSqrt(-5);
+    double b = sumSqrt(3);
     double c = sumSqrt(5);
     double d = sumSqrt(2);
     double e = sumSqrt(1);
