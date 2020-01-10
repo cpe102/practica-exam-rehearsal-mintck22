b4bf7fef59da61f6dd85cd13a3da72b853afc3a2
Mint <chawanluck_k@hotmail.com>
2020-01-10 17:08:52 +0700

    2

--- rehearsal_2.cpp
+++ rehearsal_2.cpp
@@ -1,1 +1,31 @@
+#include<iostream>
+#include<string>
 
+using namespace std;
+
+int main(){
+
+    float sum = 0;
+    float x ;
+
+    cout << "Enter x: ";
+    cin >> x;
+    while (x != 0){
+    
+        if(x != 0){
+            if(x > 0){
+            sum = sum + x;
+            }
+        
+        }else{
+            cout << "sum = 0";
+            }
+
+        cout << "Enter x: ";
+        cin >> x;
+    }
+    cout << "sum = " << sum;
+
+    
+    
+}
